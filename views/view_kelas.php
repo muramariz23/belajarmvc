089b4090447df5d38788c8ab7ae2974e145bddbf
muramariz23 <m.rizkiramadhan2003@gmail.com>
2021-08-31 13:57:12 +0700

    PERTEMUAN 7 (mengganti tombol view & delete menjadi icon serta menambahkan css

--- views/view_kelas.php
+++ views/view_kelas.php
@@ -9,13 +9,29 @@ $GetKelas = $kelas->GetData_All();
 //echo var_dump($kelas);
 
  ?>
+ 		<!-- icon tugas pertemuan 7 -->
+ 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 		<!-- css tambahan -->
+ 		<link rel="stylesheet" type="text/css" href="../assets/navbar.css">
 
 
+ 		<div class="navbar">
+ 			<h1 class="nav-head">BELAJAR <span>MVC</span></h1>
+ 			<nav class="nav-body">
+ 				<ul class="listhead" style="list-style: none;">
+ 					<li><a href="view_siswa.php" class="listbody" style="background-color: #2E86C1">SISWA</a></li>
+ 					<li><a href="view_kelas.php" class="listbody" style="background-color: #6C3483">KELAS</a></li>
+ 					<li><a href="view_petugas.php" class="listbody" style="background-color: #A93226">PETUGAS</a></li>
+ 					<li><a href="view_spp.php" class="listbody" style="background-color: #229954">SPP</a></li>
+ 					<li><a href="view_pembayaran.php" class="listbody" style="background-color: #BA4A00">PEMBAYARAN</a></li>
+ 				</ul>
+ 			</nav>
+ 		</div>
 
 
  		<h1>OOP - Class, Objek, Property, Method With <u>MVC</u></h1>
  		<h2>CRUD dan CSRF</h2>
- 		<h3>Tabel Kelas</h3> <h3><a href="view_post_kelas.php">ADD DATA</a></h3>
+ 		<h3>Tabel Kelas</h3> <h3><a href="view_post_kelas.php"><i class="fa fa-plus-square" style="font-size:25px; color: blue;"> ADD DATA</i></a></h3>
 
 
 
@@ -42,9 +58,10 @@ $GetKelas = $kelas->GetData_All();
  							
  							 <!-- //untuk tindakan -->
  							 <td>
- 							 	<a href="../views/view_put_kelas.php?id_kelas=<?php echo base64_encode($Get['id_kelas']) ?>">VIEW | </a>
+ 							 	<a href="../views/view_put_kelas.php?id_kelas=<?php echo base64_encode($Get['id_kelas']) ?>"><i class="fa fa-eye" style="font-size:24px; color: green;"> </i></a>
+ 							 	<a> | </a>
  							
- 							 	<button onclick="konfirmasi()">Delete</button>
+ 							 	<button onclick="konfirmasi(<?php echo $Get['id_kelas']; ?>)"><i class="fa fa-trash" style="font-size:25px; color: red;"> </i></button>
  							 </td>
 
  						</tr>
@@ -55,7 +72,7 @@ $GetKelas = $kelas->GetData_All();
  		</table>
 
  		<script>
- 			function konfirmasi() {
+ 			function konfirmasi(id_kelas) {
  				
  				if (window.confirm('Apakah Data Ini Akan Dihapus??')) {
  					window.location.href='../config/routes.php?function=delete_kelas&id_kelas=<?php echo base64_encode($Get['id_kelas']) ?>';
--- views/view_petugas.php
+++ views/view_petugas.php
@@ -10,12 +10,29 @@ $GetPetugas = $petugas->GetData_All();
 
  ?>
 
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
+
+ 		<link rel="stylesheet" type="text/css" href="../assets/navbar.css">
+
+
+ 		<div class="navbar">
+ 			<h1 class="nav-head">BELAJAR <span>MVC</span></h1>
+ 			<nav class="nav-body">
+ 				<ul class="listhead" style="list-style: none;">
+ 					<li><a href="view_siswa.php" class="listbody" style="background-color: #2E86C1">SISWA</a></li>
+ 					<li><a href="view_kelas.php" class="listbody" style="background-color: #6C3483">KELAS</a></li>
+ 					<li><a href="view_petugas.php" class="listbody" style="background-color: #A93226">PETUGAS</a></li>
+ 					<li><a href="view_spp.php" class="listbody" style="background-color: #229954">SPP</a></li>
+ 					<li><a href="view_pembayaran.php" class="listbody" style="background-color: #BA4A00">PEMBAYARAN</a></li>
+ 				</ul>
+ 			</nav>
+ 		</div>
 
  		<h1>OOP - Class, Objek, Property, Method With <u>MVC</u></h1>
  		<h2>CRUD dan CSRF</h2>
- 		<h3>Tabel petugas</h3> <h3><a href="view_post_petugas.php">ADD DATA</a></h3>
+ 		<h3>Tabel petugas</h3> <h3><a href="view_post_petugas.php"><i class="fa fa-plus-square" style="font-size:25px; color: blue;"> ADD DATA</i></a></h3>
 
 
 
@@ -47,9 +64,10 @@ $GetPetugas = $petugas->GetData_All();
 
  							 <!-- //untuk tindakan -->
  							<td>
- 							 	<a href="../views/view_put_petugas.php?id_petugas=<?php echo base64_encode($Get['id_petugas']) ?>">VIEW | </a>
+ 							 	<a href="../views/view_put_petugas.php?id_petugas=<?php echo base64_encode($Get['id_petugas']) ?>"><i class="fa fa-eye" style="font-size:24px; color: green;"> </i></a>
+ 							 	<a> | </a>
  							
- 							 	<button onclick="konfirmasi()">Delete</button>
+ 							 	<button onclick="konfirmasi(<?php echo $Get['id_petugas']; ?>)"><i class="fa fa-trash" style="font-size:25px; color: red;"> </i></button>
  							 </td>
 
  						</tr>
